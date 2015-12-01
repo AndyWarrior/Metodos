ea1f79cd83982701930babd220bd902e111643d8
Eliezer Galvan <egalvan10@gmail.com>
2015-12-01 17:46:29 -0600

    Read initial data from files, create CSV reports

--- Metodos/main.cpp
+++ Metodos/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <math.h>
 
 #define TOTAL_ITERATIONS  10000000
@@ -14,11 +15,46 @@
 int clients = 0;
 int iterations = 0;
 double time_passed = 0.0;
+int frames[2000], delays[2000];
 
 using namespace std;
 
+void loadInitialData()
+{
+	int lines = 2000;
+	
+	//Load frames from file
+	ifstream infile;
+	infile.open("frames.txt");
+	for(int i=0; i<lines; i++){
+		infile >> frames[i];
+	}
+	infile.close();
+	
+	//Load delays from file
+	infile.open("delays.txt");
+	for(int i=0; i<lines; i++){
+		infile >> delays[i];
+	}
+	infile.close();
+}
+
+void generateCSV(string filename, string contents)
+{
+	//Generate report as CSV
+	ofstream outfile;
+	outfile.open(filename);
+	outfile << contents;
+	outfile.close();
+}
+
 int main(int argc, const char * argv[]) {
-    
+	
+	//Load frames and delays from txt files
+	loadInitialData();
+	cout << "Initial data loaded..." << endl;
+	
+	//Start simulation
     while (iterations <= TOTAL_ITERATIONS) {
         if (iterations % 222 == 0) {
             cout << "Departure time: " << iterations << " microseconds" << endl;
@@ -34,5 +70,11 @@ int main(int argc, const char * argv[]) {
         
         iterations++;
     }
+	
+	//Generate CSV files from contents
+	string contents = "hello,there\nlitte,world";
+	generateCSV("results.csv", contents);
+	cout << "Report generated..." << endl;
+	
     return 0;
 }
